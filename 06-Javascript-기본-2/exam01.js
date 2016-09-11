6dbd5342db5d3ead0618ffe2994e8b839f301557
t1t1 <cha@hanmail2.net>
2016-09-11 11:37:45 +0900

    06-Javascript-기본-2

---
+++ 06-Javascript-기본-2/exam01.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang='ko'>
+<head>
+
+
+<script type="text/javascript">
+// 오브젝트 타입을 문자열로 반화나는 공통함수를 생성하라
+// 오브젝트 대상 : Object, Number, Array, String
+	
+	var obj = new Object();
+	var str = new String();
+	var arr = new Array();
+	var num = new Number();
+
+	/*/
+	Object.prototype.toString = 
+
+	/**/
+
+	console.log(obj.toString());
+	console.log(str.toString());
+	console.log(arr.toString());
+	console.log(num.toString());
+
+</script>
+</head>
+<body>
+</body>
+<html>
