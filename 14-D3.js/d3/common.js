c791ecec55d5bc4b3026e145a219aa62cdb42fb2
t1t1 <cha@hanmail2.net>
2016-09-11 14:22:12 +0900

    14-D3.js || add src

---
+++ 14-D3.js/d3/common.js
@@ -0,0 +1,22 @@
+
+//
+var app = require('express')();
+var url = require('url');
+
+//
+app.get('/', function(req, res) {
+  var fileName = 'csv.html'; //
+  res.sendFile(fileName, {root: __dirname});
+  console.log('get: ' + fileName);
+});
+
+//
+app.use(function(req, res) {
+  var fileName = url.parse(req.url).pathname.replace('/', ''); //
+  res.sendFile(fileName, {root: __dirname});
+  console.log('use: ', fileName);
+});
+
+//
+app.listen(3000);
+console.log('server on port 3000');
